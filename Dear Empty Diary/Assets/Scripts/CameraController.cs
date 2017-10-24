5882058a00a8f2ade87895fe50496bace97ee20f
Johnny Mak <johnny_mak@outlook.com>
2017-10-24 15:43:26 -0400

    Added Ruby Walking Mechanic and Animation + Simple Door Animation

--- Dear Empty Diary/Assets/Scripts/CameraController.cs
+++ Dear Empty Diary/Assets/Scripts/CameraController.cs
@@ -9,17 +9,17 @@ public class CameraController : MonoBehaviour {
 	// On Button Press - Move Camera Pan
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < ROOM_SIZE_PAN)
-        {
-            Vector3 newPos = this.transform.position;
-            newPos.x = ROOM_SIZE_PAN;
-            this.transform.position = newPos;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > 0.0f)
-        {
-            Vector3 newPos = this.transform.position;
-            newPos.x = 0;
-            this.transform.position = newPos;
-        }
+    //    if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < ROOM_SIZE_PAN)
+    //    {
+    //        Vector3 newPos = this.transform.position;
+    //        newPos.x = ROOM_SIZE_PAN;
+    //        this.transform.position = newPos;
+    //    }
+    //    else if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > 0.0f)
+    //    {
+    //        Vector3 newPos = this.transform.position;
+    //        newPos.x = 0;
+    //        this.transform.position = newPos;
+    //    }
     }
 }
---
+++ Dear Empty Diary/Assets/Scripts/RubyWalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyWalk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movement();
+
+	}
+
+    void movement()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector2.right * 2 * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 2 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * 2 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector2.up * 2 * Time.deltaTime);
+        }
+
+    }
+}
