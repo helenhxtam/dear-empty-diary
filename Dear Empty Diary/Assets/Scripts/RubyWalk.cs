ec54eea7bfc532b13f07e6760744de5e6aae2e93
Jason-PC\Jason <kalecj@hotmail.com>
2017-10-30 02:25:59 -0400

    Almost finished ruby walking animations for all directions, small bugs if multiple buttons pressed at same time

--- Dear Empty Diary/Assets/Scripts/RubyWalk.cs
+++ Dear Empty Diary/Assets/Scripts/RubyWalk.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyWalk : MonoBehaviour {
-
     // The direction she's facing
     private Vector2 direction;
 
-	// FixedUpdate is called once per frame
-	void FixedUpdate () {
+    //animator for characters movement
+    Animator animator;
+
+    //booleans to switch state of animator
+    [SerializeField]
+    bool isMoving;
+    [SerializeField]
+    bool isForward;
+    [SerializeField]
+    bool isBack;
+    [SerializeField]
+    bool isLeft;
+    [SerializeField]
+    bool isRight;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate () {
+        if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            isMoving = false;
+            
+        }
         Movement();
-	}
+
+        animator.SetBool("forward", isForward);
+        animator.SetBool("back", isBack);
+        animator.SetBool("left", isLeft);
+        animator.SetBool("right", isRight);
+        animator.SetBool("moving", isMoving);
+    }
 
     // Keyboard controls to move Ruby
     void Movement()
@@ -21,24 +51,44 @@ public class RubyWalk : MonoBehaviour {
             {
                 transform.Translate(-Vector2.right * 2 * Time.deltaTime);
                 direction = -Vector2.right;
+                isForward = false;
+                isBack = false;
+                isLeft = true;
+                isRight = false;
+                isMoving = true;
             }
 
             if (Input.GetKey(KeyCode.D))
             {
                 transform.Translate(Vector2.right * 2 * Time.deltaTime);
                 direction = Vector2.right;
+                isForward = false;
+                isBack = false;
+                isLeft = false;
+                isRight = true;
+                isMoving = true;
             }
 
             if (Input.GetKey(KeyCode.W))
             {
                 transform.Translate(Vector2.up * 2 * Time.deltaTime);
                 direction = Vector2.up;
+                isForward = false;
+                isBack = true;
+                isLeft = false;
+                isRight = false;
+                isMoving = true;
             }
 
             if (Input.GetKey(KeyCode.S))
             {
                 transform.Translate(-Vector2.up * 2 * Time.deltaTime);
                 direction = -Vector2.up;
+                isForward = true;
+                isBack = false;
+                isLeft = false;
+                isRight = false;
+                isMoving = true;
             }
         }
     }
