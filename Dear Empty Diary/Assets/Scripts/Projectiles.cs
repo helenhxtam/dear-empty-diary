6073edeba9dcd588fcd002419b9e07d77e6c379b
Johnny Mak <jmkz95@hotmail.com>
2017-11-17 20:13:38 -0500

    Added Walls in Room 5 which can be shot through

--- Dear Empty Diary/Assets/Scripts/Projectiles.cs
+++ Dear Empty Diary/Assets/Scripts/Projectiles.cs
@@ -6,7 +6,14 @@ public class Projectiles : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+        // Finds all GameObjects in the scene with the tag Fence
+        GameObject[] fences = GameObject.FindGameObjectsWithTag("Fence");
+
+        // For each Fence, ignore collision with it
+        foreach (GameObject obj in fences) {
+            Physics2D.IgnoreCollision(obj.gameObject.GetComponent<BoxCollider2D>(), this.GetComponent<CircleCollider2D>(), true);
+        }
 	}
 	
 	// Update is called once per frame
@@ -22,8 +29,7 @@ public class Projectiles : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Levers")
-        {
+        if (col.gameObject.tag == "Levers") {
             Destroy(this.gameObject);
         }
     }
