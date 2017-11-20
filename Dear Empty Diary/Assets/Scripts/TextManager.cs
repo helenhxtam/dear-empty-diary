e47571fcdab12ae78b6973dadb61a03be2087b88
Philip Michael <philmichaelmusic@gmail.com>
2017-11-20 16:35:29 -0500

    Added functional text control. Utilizes a TextManager and TextScript which has a PlayText flag. Please refer to the comments (in the appropriate regions) for how to use it (comments are in TextManager.cs and TextScript.cs). For future reference, we need a TextManager in the scene, and then attach the TextScript to the appropriate objects that utilizes them (by so doing, add them to the prefabs). The flag there will determine if text plays or not (similarly if text even exists on them).

---
+++ Dear Empty Diary/Assets/Scripts/TextManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    #region TextManager Description
+    /*
+     * The Text Manager will handle outputting Text to the dialogue box area. 
+     * To use: 
+     *      1. Ensure you have an instance of TextManager in the scene.
+     *      2. Add a TextScript to any triggerable object or event to trigger text. Check the 'PlayText' if you wish to play text for that object.
+     *      3. The TextScript will communicate with the TextManager and pass the assigned text.
+     *      4. After communicating, it will output the text you wrote in the TextScript's field to the dialogue box.
+     *      5. Finally, button presses will progress with the dialogue in-order of the array.
+    */
+    #endregion
+
+    #region Variables
+    [SerializeField]
+    private GameObject textBox; // The dialogue box panel (called 'Text Box')
+    [SerializeField]
+    private GameObject dialogueBox; // The text box attached to the panel (called 'Dialogue')
+    private Text dialogueText; // The text associated to the dialogue box
+    private string[] text; // The text passed to the TextManager to use
+    private int textLength; // The number of entries in the text[] array
+    private int counter = 0; // By default, start at entry 0 in the text array
+    #endregion
+
+    #region TextManager Functions
+    void Start()
+    {
+        // Fetches the dialogue box's text component
+        this.dialogueText = dialogueBox.GetComponent<Text>();
+        ToggleTextBox();
+    }
+
+    // Function that takes in text and outputs it to the dialogue box
+    public void WriteText(string[] script)
+    {
+        // Set the counter back to 0, text's max length, the text array, and print out the first entry
+        this.counter = 0;
+        ToggleTextBox();
+        this.textLength = script.Length - 1; // -1 because of array uses (i.e. an array of 2 elements returns length of 2, but we use element 0 and 1)
+        this.text = script;
+        this.dialogueText.text = this.text[this.counter];
+    }
+
+    // Toggles the text box (on if off, off if on)
+    public void ToggleTextBox()
+    {
+        this.textBox.SetActive(!this.textBox.activeSelf);
+        this.dialogueBox.SetActive(!this.dialogueBox.activeSelf);
+    }
+
+    // Progresses automatically to the next dialogue in the array (if it exists)
+    public void ProgressThroughDialogueAutomatically()
+    {
+        // If there is still some text to output after pressing OK
+        if (counter < textLength)
+        {
+            // Increment our counter, and then print out the text
+            this.counter++;
+            this.dialogueText.text = this.text[this.counter];
+        }
+        else
+        {
+            // Otherwise, toggle the text box on pressing OK
+            ToggleTextBox();
+        }
+    }
+    #endregion
+
+}
---
+++ Dear Empty Diary/Assets/Scripts/TextScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour
+{
+
+    #region TextScript Description
+    /*
+     * The TextScript objected is attached to every interactable object that would trigger text (or door if you use it that way).
+     * When an object is interacted with (or would trigger text, i.e. by colliding with it), simply call a function here that communicates with the TextManager.
+     * It then processes writing the text.
+    */
+    #endregion
+
+    #region Variables
+    [Tooltip("The text we wish to print out.")]
+    public string[] dialogueText; // The dialogue text we'll be sending over, line by line, to the TextManager.
+    [SerializeField]
+    private GameObject textManager; // The Text Manager object - if we don't assign it, automatically find it.
+    [Tooltip("Boolean to determine whether or not, on collision/trigger, we output text.")]
+    public bool playText;
+    #endregion
+
+    #region TextScript functions
+    void Start()
+    {
+        // Verify that a TextManager was attached, else attach it (find it)
+        if (!this.textManager)
+        {
+            this.textManager = GameObject.Find("TextManager");
+        }
+    }
+
+    // On trigger OR collision, handle checking if we play text at all.
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (playText)
+        {
+            TriggerDialogue();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (playText)
+        {
+            TriggerDialogue();
+        }
+    }
+    // -------------------------
+
+    // Function to communicate with the TextManager by passing to it the dialogue text array.
+    // Every other functionality is handled through the button in the canvas.
+    public void TriggerDialogue()
+    {
+        textManager.GetComponent<TextManager>().WriteText(dialogueText);
+    }
+    #endregion
+}
