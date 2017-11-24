17de516d0ba3058f627b2cf7f90bc2baae9a4041
Johnny Mak <jmkz95@hotmail.com>
2017-11-24 16:53:00 -0500

    Adjusted Room and Door Colliders

---
+++ Dear Empty Diary/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+
+    [Tooltip("The sprite that a closed door has.")]
+    public Sprite closedDoor;
+
+    [Tooltip("The sprite that an open door has.")]
+    public Sprite openDoor;
+
+    void OpenDoor() {
+
+    }
+
+    void CloseDoor() {
+
+    }
+}
